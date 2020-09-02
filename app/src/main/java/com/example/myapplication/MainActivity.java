902dc8815481cad0fe2103dbf953348afb4f72b1
gardeeen <akk794@naver.com>
2020-09-02 23:23:01 +0900

    정원 Sub1Activity layout-activity_sub1, content_sub1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 Intent intent = new Intent(
                         getApplicationContext(),
-                        OnairActivity.class);
+                        Sub1Activity.class);
                 startActivity(intent);
             }
         });
