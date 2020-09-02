9066b19a8e0cdc810faf7c5b9c6aba1ef1f63082
gardeeen <akk794@naver.com>
2020-09-02 23:52:09 +0900

    정원 TodayActivity, TodayView layout- activity_today, today_item drawable-border_listlayout.xml 추가

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 Intent intent = new Intent(
                         getApplicationContext(),
-                        Sub1Activity.class);
+                        TodayActivity.class);
                 startActivity(intent);
             }
         });
